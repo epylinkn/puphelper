897d4528ac526f5b283ddfddee68bbfb2fb68cf5
Cat <catyundo@gmail.com>
2021-07-16 19:04:36 -0700

    Add dog resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Dog.create(name: 'Nana', sex: 'F', breed: 'Golden Retriever', current_weight: 55, birthdate: "06/05/2012", age: 9)
+Dog.create(name: 'Boomer', sex: 'M', breed: 'Affenpinscher Mix', current_weight: 28, birthdate: "10/10/20", age: 1)
+Dog.create(name: 'Pigeon', sex: 'F', breed: 'Poodle', current_weight: 45, birthdate: "02/14/2020", age: 1)
