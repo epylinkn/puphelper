35c3a55ce5783ae7eb6799607d0f6f1f9d159ead
Anthony Bui <anthonybui@gmail.com>
2021-07-16 22:12:36 -0700

    :tada: Working react setup!

---
+++ app/javascript/components/DogRoot.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DogRoot = () => (
+  <div className="container">
+    <p>
+      <strong>Name:</strong>
+      BOOMER
+    </p>
+  </div>
+);
+
+export default DogRoot;
---
+++ app/javascript/packs/sauce.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "react-dom";
+import DogRoot from "../components/DogRoot";
+
+document.addEventListener("DOMContentLoaded", () => {
+  render(<DogRoot />, document.body.appendChild(document.createElement("div")));
+});
